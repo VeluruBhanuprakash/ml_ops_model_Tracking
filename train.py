fe486c013a2bafb133216769b7aded1ff0a7613c
VeluruBhanuprakash <70476493+VeluruBhanuprakash@users.noreply.github.com>
2021-09-18 13:30:54 +0530

    Update train.py

--- train.py
+++ train.py
@@ -27,9 +27,9 @@ imp.fit(X)
 X = imp.transform(X)
 
 
-# Linear model
+# model
 clf = LogisticRegression()
-yhat = cross_val_predict(clf, X, y, cv=5)
+yhat = cross_val_predict(clf, X, y, cv=10)
 
 acc = np.mean(yhat==y)
 tn, fp, fn, tp = confusion_matrix(y, yhat).ravel()
